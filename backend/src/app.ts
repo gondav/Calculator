845742e30b75f57ea7c92eb459d8633e7a3b66ff
gondav <viktoria.gonda@gmail.com>
2022-11-08 18:08:28 +0100

    import numberRouter and create /api endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import { numberRouter } from './routes/number.routes';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('/api', numberRouter);
 
 export default app;
