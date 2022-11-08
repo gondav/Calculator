00bdb04ed396c51dcfedb1ba3f85b23bd523bb22
gondav <viktoria.gonda@gmail.com>
2022-11-08 18:20:37 +0100

    errorHandler imported and added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import { numberRouter } from './routes/number.routes';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use('/api', numberRouter);
+
+app.use(errorHandler);
 
 export default app;
