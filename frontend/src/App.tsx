0e46ea2d9956790307c7c35d54d935a9dcd400fb
gondav <viktoria.gonda@gmail.com>
2022-11-08 23:40:34 +0100

    add basic html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,28 @@
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='calculator-grid'>
+      <div className='output'>(0) 0</div>
+      <button>MR</button>
+      <button>MS</button>
+      <button>AC</button>
+      <button>X</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>/</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
+    </div>
+  );
 }
 
 export default App;
